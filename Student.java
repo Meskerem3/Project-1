f75cced149f3b7dbec4f9e27e1e5ff6e2206127d
Meskerem3 <147622717+Meskerem3@users.noreply.github.com>
2023-11-04 11:50:01 -0700

    Update Student.java
    In this code i used the setter and getter method but i should not have to use it so i update the code as the instruction

--- Student.java
+++ Student.java
@@ -1,15 +1,6 @@
 public class Student extends Person {
-    private String classroom;
-
-    public String getClassroom() {
-        return classroom;
-    }
-
-    public void setClassroom(String classroom) {
-        this.classroom = classroom;
-    }
-
-    public Student(int id, String name, int age, boolean parentPermission, String classroom) {
+    private String classroom;  
+       public Student(int id, String name, int age, boolean parentPermission, String classroom) {
         super(id, name, age, parentPermission);
         this.classroom = classroom;
     }
