b402b8b367428558584cc6b29c9c829e611cab2b
Your Name <you@example.com>
2023-12-05 03:25:01 -0800

    this is weeek 4

---
+++ App.java
@@ -0,0 +1,32 @@
+
+public class App {
+    // Method to list all books
+    public void listAllBooks() {
+
+    }
+
+    // Method to list all people
+    public void listAllPeople() {
+
+    }
+
+    // Method to create a person (teacher or student)
+    public void createPerson(String type) {
+
+    }
+
+    // Method to create a book
+    public void createBook() {
+
+    }
+
+    // Method to create a rental
+    public void createRental() {
+
+    }
+
+    // Method to list all rentals for a given person id
+    public void listRentalsForPerson(int personId) {
+
+    }
+}
